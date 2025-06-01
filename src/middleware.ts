f919ea16c0ce969bf9241c4f585155ec2b20e134
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2025-06-01 14:21:45 +0800

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,8 @@ import type { NextRequest } from 'next/server'
 export async function middleware(req: NextRequest) {
     const res = NextResponse.next()
 
-    // Create a Supabase client configured to use cookies
     const supabase = createMiddlewareClient({ req, res })
 
-    // Refresh session if expired - required for Server Components
     const { data: { session } } = await supabase.auth.getSession()
     if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
         return NextResponse.redirect(new URL('/login', req.url))
