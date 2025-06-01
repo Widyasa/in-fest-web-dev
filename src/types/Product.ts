8ca04b77259c0ee26fe8e2889b5b5926d4e6bbd4
Widya Yasa <108384245+Widyasa@users.noreply.github.com>
2025-06-01 14:54:02 +0800

    fix: fix crud product type

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -11,8 +11,23 @@ export interface Product {
   category_name?: string;  
 }
 
-export type NewProduct = Omit<Product, 'id' | 'created_at'>;
-
-export type UpdateProduct = Partial<Omit<Product, 'id' | 'created_at'>> & {
-  id: number;
+export type NewProduct = {
+  name?: string;
+  price?: number;
+  img_link?: string;
+  shop_link?: string;
+  ingredients?: string[];
+  features?: string[];
+  category_id?: number;
 };
+
+export type UpdateProduct = {
+  id?: number;
+  name?: string;
+  price?: number;
+  img_link?: string;
+  shop_link?: string;
+  ingredients?: string[];
+  features?: string[];
+  category_id?: number;
+}
